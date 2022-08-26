8424f58b1b6b3275ceebf10a738d45a562489eab
hyugadayo <achano.0616@gmail.com>
2022-08-26 19:55:37 +0900

    kind_user_aggregator修正

--- aggregator/kind_user_aggregator.rb
+++ aggregator/kind_user_aggregator.rb
@@ -21,9 +21,9 @@ class KindUserAggregator
   
   users = []
   reactions.map{ |reaction|
-    users << reaction.map{ |data| data["users"]}
+    users << reaction.map{ |x| x["users"]}
   }
-  
+
   users = users.flatten.group_by(&:itself)
 
   datas = []
